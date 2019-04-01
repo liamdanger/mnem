b4bb2613b701edfb4edd18b3404c396735261a5e
Liam Campbell <hi@liamcampbell.info>
2019-03-31 17:47:03 -0700

    Use commandDir because it's neato

--- commands/add.js
+++ commands/add.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 module.exports = {
     command: 'add <key> [value]',
     aliases: ['a', 'update', 'u'],
-    desc: 'Remember a string at provided key (or update existing key)',
+    desc: 'Remember a string at provided key',
     handler: async ({key, value }) => {
         await storage.init()
         await storage.setItem(key, value)
--- index.js
+++ index.js
@@ -2,15 +2,8 @@
 
 const yargs = require('yargs')
 
-const add = require('./commands/add')
-const remove = require('./commands/remove')
-const get = require('./commands/get')
-const list = require('./commands/list')
-
 return yargs
-    .command(add)
-    .command(remove)
-    .command(get)
-    .command(list)
+    .commandDir('commands')
+    .demandCommand()
     .help()
     .argv
